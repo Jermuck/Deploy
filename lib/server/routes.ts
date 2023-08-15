d30ad24fc3e7cf57ad1ab8fcd2d3642ad9f2cb0c
Jermuck <k.bardyugov@yandex.ru>
2023-08-15 19:30:42 +0500

    add test endpoint

--- lib/server/routes.ts
+++ lib/server/routes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { Controllers } from 'lib/types/controllers';
+import {Request, Response} from 'express';
 
 export function makeRouter({ controllers }: { controllers: Controllers }) {
   const router = express.Router();
@@ -7,6 +8,8 @@ export function makeRouter({ controllers }: { controllers: Controllers }) {
   router.get('/address/autocomplete', controllers.autocompleteAddressController);
   router.get('/property/info', controllers.getPropertyInfoController);
   router.post('/estimate', controllers.estimateController);
-
+  router.post( '/',(_: Request, res: Response) => {
+    res.send('Server work...');
+  });
   return router;
 }
